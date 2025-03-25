84b8ba9c07ba39d91f6cc8ed18cf68e6d1445419
Özge Boztaş <123666735+ozgeboztass@users.noreply.github.com>
2025-03-25 05:34:26 +0300

    **

---
+++ contracts/GovernanceToken.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract GovernanceToken is ERC20Votes {
+    constructor(string memory name, string memory symbol)
+        ERC20(name, symbol)
+        ERC20Permit(name)
+    {
+        _mint(msg.sender, 1000000 * 10 ** decimals()); // İlk arz
+    }
+
+    // Aşağıdaki işlevler ERC20Votes tarafından sağlanır ve geçersiz kılınmalıdır
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
+}
